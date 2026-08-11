da5f314fe3a828c1b6f34346912a51ce002e3201
prathameshbhagat <prathameshbhagat0608@gmail.com>
2026-08-11 14:13:53 +0530

    First git commit for spring boot

---
+++ src/main/java/com/hindi4all/h4j/H4jApplication.java
@@ -0,0 +1,13 @@
+package com.hindi4all.h4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hindi4all/h4j/H4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hindi4all.h4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
